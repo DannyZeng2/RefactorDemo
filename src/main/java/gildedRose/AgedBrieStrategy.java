7826b3308543f9a761cd00e5f1d68aa28ae1613b
DannyZeng <15625140174@163.com>
2019-08-12 23:07:42 +0800

    [refactor] 将条件判断的逻辑重构到AgedBrieStrategy，并通过测试

--- src/main/java/gildedRose/AgedBrieStrategy.java
+++ src/main/java/gildedRose/AgedBrieStrategy.java
@@ -3,7 +3,13 @@ package gildedRose;
 public class AgedBrieStrategy {
 
     public void updateQualityAndSellIn(Item item){
-
+        if(item.quality<50){
+            item.quality++;
+        }
+        item.sellIn--;
+        if(item.sellIn<0&&item.quality<50){
+            item.quality ++;
+        }
     }
 
 }
