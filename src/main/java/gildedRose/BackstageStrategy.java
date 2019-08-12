27c3612013ae819ebef72637f0b938d63ce6850d
DannyZeng <15625140174@163.com>
2019-08-12 23:08:44 +0800

    [refactor] 将条件判断的逻辑重构到BackstageStrategy，并通过测试

--- src/main/java/gildedRose/BackstageStrategy.java
+++ src/main/java/gildedRose/BackstageStrategy.java
@@ -2,6 +2,18 @@ package gildedRose;
 
 public class BackstageStrategy {
     public void updateQualityAndSellIn(Item item){
-
+        if(item.quality < 50){
+            item.quality++;
+            if(item.sellIn < 11 && item.quality < 50){
+                item.quality++;
+            }
+            if(item.sellIn < 6 && item.quality < 50){
+                item.quality++;
+            }
+        }
+        item.sellIn--;
+        if(item.sellIn < 0){
+            item.quality = 0;
+        }
     }
 }
