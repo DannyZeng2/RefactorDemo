99b6988260a855d3db7a1f2df33b8466e745f802
DannyZeng <15625140174@163.com>
2019-08-12 23:09:36 +0800

    [refactor] 将条件判断的逻辑重构到OtherStrategy，并通过测试

--- src/main/java/gildedRose/OtherStrategy.java
+++ src/main/java/gildedRose/OtherStrategy.java
@@ -3,6 +3,14 @@ package gildedRose;
 public class OtherStrategy {
 
     public void updateQualityAndSellIn(Item item){
-
+        if(item.quality>0){
+            item.quality--;
+        }
+        item.sellIn--;
+        if(item.sellIn<0){
+            if(item.quality>0){
+                item.quality--;
+            }
+        }
     }
 }
