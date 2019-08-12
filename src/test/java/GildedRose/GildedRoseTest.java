189869216ca6376aa88af831de521f5cfc4b7a81
DannyZeng <15625140174@163.com>
2019-08-12 22:11:21 +0800

    [test] 添加测试，当name为other，sellIn为5，quality为5

--- src/test/java/GildedRose/GildedRoseTest.java
+++ src/test/java/GildedRose/GildedRoseTest.java
@@ -6,8 +6,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class GildedRoseTest {
-
-
-
+    @Test
+    public void should_update_other_when_sellIn_is_5_and_quality_is_5() {
+        Item item = new Item("Other",5,5);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+        gildedRose.updateQuality();
+        Assert.assertEquals("Other, 4, 4",gildedRose.getItems()[0].toString());
+    }
 
 }
