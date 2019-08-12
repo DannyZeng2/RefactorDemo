4ac8e78dc3299edf3dc666813e98e546f28255e0
DannyZeng <15625140174@163.com>
2019-08-12 22:11:36 +0800

    [test] 添加测试，当name为other，sellIn为-1，quality为5

--- src/test/java/GildedRose/GildedRoseTest.java
+++ src/test/java/GildedRose/GildedRoseTest.java
@@ -14,4 +14,12 @@ public class GildedRoseTest {
         Assert.assertEquals("Other, 4, 4",gildedRose.getItems()[0].toString());
     }
 
+    @Test
+    public void should_update_sellIn_when_name_is_sulfuras_hand_of_ragnaros_() {
+        Item item = new Item("Other",-1,5);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+        gildedRose.updateQuality();
+        Assert.assertEquals("Other, -2, 3",gildedRose.getItems()[0].toString());
+    }
+
 }
